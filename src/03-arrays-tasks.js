8f1777d97889a39c9e54766e84f9e0abe0a4a5cc
Kate Mashko <katika.life@gmail.com>
2023-10-23 14:09:36 +0300

    feat: add solutions

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -228,8 +228,9 @@ function toCsvText(/* arr */) {
  *   [ 0, 1, 2, 3, 4, 5 ] => [ 0, 1, 4, 9, 16, 25 ]
  *   [ 10, 100, -1 ]      => [ 100, 10000, 1 ]
  */
-function toArrayOfSquares(/* arr */) {
-  throw new Error('Not implemented');
+function toArrayOfSquares(arr) {
+  const newArr = arr.map((item) => item * item);
+  return newArr;
 }
 
 
@@ -247,8 +248,9 @@ function toArrayOfSquares(/* arr */) {
  *   [ 0, 0, 0, 0, 0]         => [ 0, 0, 0, 0, 0]
  *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
  */
-function getMovingSum(/* arr */) {
-  throw new Error('Not implemented');
+function getMovingSum(arr) {
+  const newArr = arr.map((item, index) => index - 1 + index);
+  return newArr;
 }
 
 /**
@@ -262,8 +264,10 @@ function getMovingSum(/* arr */) {
  * [ 'a', 'b', 'c' , null ]  => [ "b", null ]
  * [ "a" ] => []
  */
-function getSecondItems(/* arr */) {
-  throw new Error('Not implemented');
+function getSecondItems(arr) {
+  const newArr = arr.filter((item, index) => index % 2 !== 0);
+
+  return newArr;
 }
 
 
